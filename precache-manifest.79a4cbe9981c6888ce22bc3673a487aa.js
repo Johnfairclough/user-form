8d6e7fcec6f41c471ca17a3f70ffa9e9928d81a4
John Fairclough <johnmfairclough@gmail.com>
2020-04-09 14:14:35 +0100

    Updates

---
+++ precache-manifest.79a4cbe9981c6888ce22bc3673a487aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58433c3100605acf2078ade6a37f31e5",
+    "url": "/user-form/index.html"
+  },
+  {
+    "revision": "31367993656486e2de57",
+    "url": "/user-form/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6873f5c4fc6b38ba900",
+    "url": "/user-form/static/js/2.e1a428ef.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/user-form/static/js/2.e1a428ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31367993656486e2de57",
+    "url": "/user-form/static/js/main.70d9dee1.chunk.js"
+  },
+  {
+    "revision": "35a79d6d08b9e81c00b8",
+    "url": "/user-form/static/js/runtime-main.6eb41d19.js"
+  }
+]);
